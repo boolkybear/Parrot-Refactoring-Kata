7bd9bfccdf459bb8167991cad073552b3e78ed40
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-11 18:00:20 +0200

    refactor: tests use the new classes

--- PHP/tests/ParrotTest.php
+++ PHP/tests/ParrotTest.php
@@ -6,48 +6,43 @@ class ParrotTest extends \PHPUnit_Framework_TestCase{
 
   /** @test */
   public function get_speed_of_european_parrot() {
-    $parrot = $this->parrot(ParrotTypeEnum::EUROPEAN, 0,0, false);
+    $parrot = new EuropeanParrot();
     $this->assertEquals(12.0, $parrot->getSpeed());
   }
 
   /** @test */
   public function get_speed_of_african_parrot_with_one_coconut() {
-    $parrot = $this->parrot(ParrotTypeEnum::AFRICAN, 1, 0, false);
+    $parrot = new AfricanParrot(1);
     $this->assertEquals(3.0, $parrot->getSpeed());
   }
 
   /** @test */
   public function get_speed_of_african_parrot_with_two_coconuts() {
-    $parrot = $this->parrot(ParrotTypeEnum::AFRICAN, 2, 0, false);
+    $parrot = new AfricanParrot(2);
     $this->assertEquals(0.0, $parrot->getSpeed());
   }
 
   /** @test */
   public function get_speed_of_african_parrot_with_no_coconuts() {
-    $parrot = $this->parrot(ParrotTypeEnum::AFRICAN, 0,0, false);
+    $parrot = new AfricanParrot(0);
     $this->assertEquals(12.0, $parrot->getSpeed());
   }
 
   /** @test */
   public function get_speed_norwegian_blue_parrot_nailed() {
-    $parrot = $this->parrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0,0,true);
+    $parrot = new NailedNorwegianParrot();
     $this->assertEquals(0.0, $parrot->getSpeed());
   }
 
   /** @test */
   public function get_speed_norwegian_blue_parrot_not_nailed() {
-  $parrot = $this->parrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 1.5, false);
+    $parrot = new NotNailedNorwegianParrot(1.5);
     $this->assertEquals(18.0, $parrot->getSpeed());
   }
 
   /** @test */
   public function get_speed_norwegian_blue_parrot_not_nailed_high_voltage() {
-    $parrot = $this->parrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0,4, false);
+    $parrot = new NotNailedNorwegianParrot(4);
     $this->assertEquals(24.0, $parrot->getSpeed());
-  }
-
-  private function parrot($type, $numberOfCoconuts, $voltage, $isNailed){
-    $factory = new ParrotFactory();
-    return $factory->build($type, $numberOfCoconuts, $voltage, $isNailed);
   }
 }
