512feb3bb4572ee7179f68f25ab66f7578cdd846
Luis Rovirosa <luisrovirosa@gmail.com>
2016-07-11 17:39:01 +0200

    refactor: EuropeanParrot as a class

---
+++ PHP/src/EuropeanParrot.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Parrot;
+
+class EuropeanParrot extends Parrot {
+  /**
+   * EuropeanParrot constructor.
+   */
+  public function __construct() {
+  }
+
+  public function getSpeed() {
+    return 12.0;
+  }
+
+}
--- PHP/src/ParrotFactory.php
+++ PHP/src/ParrotFactory.php
@@ -5,6 +5,10 @@ namespace Parrot;
 class ParrotFactory {
 
   public function build($type, $numberOfCoconuts, $voltage, $isNailed) {
+    switch ($type){
+      case ParrotTypeEnum::EUROPEAN:
+        return new EuropeanParrot();
+    }
     return new Parrot($type, $numberOfCoconuts, $voltage, $isNailed);
   }
 }
